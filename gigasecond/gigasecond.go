26c7d9b96108ef63c5dc0952bbd5f999c391a8b4
Reggie Davis <reginald.davis88@gmail.com>
2019-06-21 16:13:14 -0500

    Refactored gigasecond solution

--- gigasecond/gigasecond.go
+++ gigasecond/gigasecond.go
@@ -1,13 +1,10 @@
 package gigasecond
 
 import (
-	"math"
 	"time"
 )
 
 // AddGigasecond receives a time, adds 1 gigasecond, and converts the time back to a date.
 func AddGigasecond(t time.Time) time.Time {
-	return t.Add(time.Duration(math.Pow10(9)) * time.Second)
+	return t.Add(1e9 * time.Second)
 }
-
-// Do I need the math package for this?
