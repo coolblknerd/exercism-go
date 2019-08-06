3450530ef05e9da231ba49752bc582d82b0720be
Reggie Davis <reginald.davis88@gmail.com>
2019-08-05 19:30:34 -0500

    Refactored raindrops exercise to remove loops

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,39 +1,29 @@
 package raindrops
 
 import (
-	"fmt"
-	"sort"
 	"strconv"
 	"strings"
 )
 
 // Convert should take a number and turn it to a string depending on the factorial of the given parameter
 func Convert(value int) string {
-
 	var drops []string
-	var keys []int
 
-	factors := map[int]string{
-		3: "Pling",
-		5: "Plang",
-		7: "Plong",
+	if value%3 == 0 {
+		drops = append(drops, "Pling")
 	}
 
-	// This will sort the keys
-	for k := range factors {
-		keys = append(keys, k)
+	if value%5 == 0 {
+		drops = append(drops, "Plang")
 	}
-	sort.Ints(keys)
 
-	for _, k := range keys {
-		if value%k == 0 {
-			fmt.Printf("%d just returned 0 for factor of %d\n", value, k)
-			drops = append(drops, factors[k])
-		}
+	if value%7 == 0 {
+		drops = append(drops, "Plong")
 	}
 
 	if len(drops) >= 1 {
 		return strings.Join(drops, "")
 	}
+
 	return strconv.Itoa(value)
 }
