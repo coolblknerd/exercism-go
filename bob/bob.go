27364503b1061d0f7c80c3d716c6c947a7294da1
Reggie Davis <reginald.davis88@gmail.com>
2019-08-05 22:05:59 -0500

    Refactored bob exercise to use switch

--- bob/bob.go
+++ bob/bob.go
@@ -1,16 +1,33 @@
 package bob
 
-import "strings"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 // Hey should have a comment documenting it.
 func Hey(remark string) string {
 	lastElement := string(remark[len(remark)-1])
-	if remark == strings.ToUpper(remark) && lastElement != "?" {
-		return "Whoa, chill out!"
-	} else if lastElement == "?" && remark != strings.ToUpper(remark) {
-		return "Sure."
-	} else if lastElement == "?" && remark == strings.ToUpper(remark) {
-		return "Calm down, I know what I'm doing!"
+	ans := "Whatever."
+	fmt.Println(lastElement)
+
+	switch lastElement {
+	default:
+		if _, err := strconv.Atoi(lastElement); err == nil {
+			ans = "Whatever."
+		} else if strings.TrimSpace(remark) == "" {
+			ans = "Fine. Be that way!"
+		} else if remark == strings.ToUpper(remark) {
+			ans = "Whoa, chill out!"
+		}
+	case "?":
+		if remark == strings.ToUpper(remark) && {
+			ans = "Calm down, I know what I'm doing!"
+		} else {
+			ans = "Sure."
+		}
 	}
-	return "Whatever."
+
+	return ans
 }
