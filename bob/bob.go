2d1390fed77be407565dc7f4f8e5d5eea585b534
Reggie Davis <reginald.davis88@gmail.com>
2019-08-13 23:38:02 -0500

    Completely refactoring and approaching the problem from a different angle

--- bob/bob.go
+++ bob/bob.go
@@ -1,43 +1,35 @@
 package bob
 
 import (
-	"fmt"
-	"strconv"
 	"strings"
-	"unicode"
 )
 
 // Hey should have a comment documenting it.
 func Hey(remark string) string {
-	str := strings.Split(remark, " ")
-	firstWord := str[0]
-	firstElement := []rune(firstWord)[0]
-	lastElement := string(remark[len(remark)-1])
+	// str := strings.Split(remark, " ")
+	// firstWord := str[0]
+	// firstElement := []rune(firstWord)[0]
+	// lastElement := string(remark[len(remark)-1])
 
-	ans := "Whatever."
-	fmt.Println("-------")
-	fmt.Println("Slice of Words: ", str)
-	fmt.Println("First Word: ", firstWord)
-	fmt.Println("First Element", firstElement)
-	fmt.Println("Last Element: ", lastElement)
-	fmt.Println("Length of Sentence: ", len(str))
+	str := strings.TrimSpace(remark)
+	// fmt.Println("-------")
+	// fmt.Println("Slice of Words: ", str)
+	// fmt.Println("First Word: ", firstWord)
+	// fmt.Println("First Element", firstElement)
+	// fmt.Println("Last Element: ", lastElement)
+	// fmt.Println("Length of Sentence: ", len(str))
 
-	switch lastElement {
+	switch {
 	default:
-		if _, err := strconv.Atoi(lastElement); err == nil {
-			ans = "Whatever."
-		} else if strings.TrimSpace(remark) == "" {
-			ans = "Fine. Be that way!"
-		} else if remark == strings.ToUpper(remark) {
-			ans = "Whoa, chill out!"
+		return "Whatever."
+	case str == "":
+		return "Fine. Be That Way"
+	case string(str[len(str)-1]) == "?":
+		if str == strings.ToUpper(str) {
+			return "Calm down, I know what I'm doing!"
 		}
-	case "?":
-		if firstWord == strings.ToUpper(firstWord) && unicode.IsLetter(firstElement) {
-			ans = "Calm down, I know what I'm doing!"
-		} else {
-			ans = "Sure."
-		}
+		return "Sure."
+	case str == strings.ToUpper(str):
+		return "Whoa, chill out!"
 	}
-
-	return ans
 }
