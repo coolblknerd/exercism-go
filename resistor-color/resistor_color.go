96b5d6f33f25091511ac204abbde7f6ec8ebef91
coolblknerd <reginald.davis88@gmail.com>
2025-03-12 10:08:03 -0500

    Added resistor color exercise

--- resistor-color/resistor_color.go
+++ resistor-color/resistor_color.go
@@ -2,10 +2,19 @@ package resistorcolor
 
 // Colors returns the list of all colors.
 func Colors() []string {
-	panic("Please implement the Colors function")
+	return []string{
+		"black", "brown", "red", "orange", "yellow",
+		"green", "blue", "violet", "grey", "white",
+	}
 }
 
 // ColorCode returns the resistance value of the given color.
 func ColorCode(color string) int {
-	panic("Please implement the ColorCode function")
+	for i, c := range Colors() {
+		if c == color {
+			return i
+		}
+	}
+
+	return -1
 }
